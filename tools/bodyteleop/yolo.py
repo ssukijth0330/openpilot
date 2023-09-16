42bb9fa2b944e80fbf75bf79b1eade06e230cc2e
Yassine <yassine.y10@gmail.com>
2023-09-16 15:19:03 -0700

    start on remote yolo runner

---
+++ tools/bodyteleop/yolo.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+import av
+import cv2
+import os
+import sys
+import argparse
+import numpy as np
+import time
+
+import cereal.messaging as messaging
+from openpilot.tools.bodyteleop.yolo_helpers import YoloRunner
+
+W, H = 1928, 1208
+V4L2_BUF_FLAG_KEYFRAME = 8
+
+# TODO: this is similar to openpilot/tools/camerastream/compressed_vipc.py
+# refactor to share code!
+def yolo_processor(addr, sock_name, yolo_runner, nvidia, debug=False):
+
+  if debug:
+    print("start decoder for %s" % sock_name)
+
+  if nvidia:
+    os.environ["NV_LOW_LATENCY"] = "3"    # both bLowLatency and CUVID_PKT_ENDOFPICTURE
+    sys.path += os.environ["LD_LIBRARY_PATH"].split(":")
+    import PyNvCodec as nvc
+
+    nvDec = nvc.PyNvDecoder(W, H, nvc.PixelFormat.NV12, nvc.CudaVideoCodec.HEVC, 0)
+    cc1 = nvc.ColorspaceConversionContext(nvc.ColorSpace.BT_709, nvc.ColorRange.JPEG)
+    conv_yuv = nvc.PySurfaceConverter(W, H, nvc.PixelFormat.NV12, nvc.PixelFormat.YUV420, 0)
+    nvDwn_yuv = nvc.PySurfaceDownloader(W, H, nvc.PixelFormat.YUV420, 0)
+    img_yuv = np.ndarray((H*W//2*3), dtype=np.uint8)
+  else:
+    codec = av.CodecContext.create("hevc", "r")
+
+  os.environ["ZMQ"] = "1"
+  messaging.context = messaging.Context()
+  sock = messaging.sub_sock(sock_name, None, addr=addr, conflate=False)
+  last_idx = -1
+  seen_iframe = False
+
+  time_q = []
+  while 1:
+    msgs = messaging.drain_sock(sock, wait_for_one=True)
+    for evt in msgs:
+      evta = getattr(evt, evt.which())
+      if debug and evta.idx.encodeId != 0 and evta.idx.encodeId != (last_idx+1):
+        print("DROP PACKET!")
+      last_idx = evta.idx.encodeId
+      if not seen_iframe and not (evta.idx.flags & V4L2_BUF_FLAG_KEYFRAME):
+        if debug:
+          print("waiting for iframe")
+        continue
+      time_q.append(time.monotonic())
+      network_latency = (int(time.time()*1e9) - evta.unixTimestampNanos)/1e6
+      frame_latency = ((evta.idx.timestampEof/1e9) - (evta.idx.timestampSof/1e9))*1000
+      process_latency = ((evt.logMonoTime/1e9) - (evta.idx.timestampEof/1e9))*1000
+
+      # put in header (first)
+      if not seen_iframe:
+        if nvidia:
+          nvDec.DecodeSurfaceFromPacket(np.frombuffer(evta.header, dtype=np.uint8))
+        else:
+          codec.decode(av.packet.Packet(evta.header))
+        seen_iframe = True
+
+      if nvidia:
+        rawSurface = nvDec.DecodeSurfaceFromPacket(np.frombuffer(evta.data, dtype=np.uint8))
+        if rawSurface.Empty():
+          if debug:
+            print("DROP SURFACE")
+          continue
+        convSurface = conv_yuv.Execute(rawSurface, cc1)
+        nvDwn_yuv.DownloadSingleSurface(convSurface, img_yuv)
+      else:
+        frames = codec.decode(av.packet.Packet(evta.data))
+        if len(frames) == 0:
+          if debug:
+            print("DROP SURFACE")
+          continue
+        assert len(frames) == 1
+        img_yuv = frames[0].to_ndarray(format=av.video.format.VideoFormat('yuv420p')).flatten()
+        uv_offset = H*W
+        y = img_yuv[:uv_offset]
+        uv = img_yuv[uv_offset:].reshape(2, -1).ravel('F')
+        img_yuv = np.hstack((y, uv))
+
+      img_rgb = cv2.cvtColor(img_yuv.reshape(H*3//2, W), cv2.COLOR_YUV2RGB_I420)
+      outs = yolo_runner.run(img_rgb)
+      # outs are YOLO outputs
+      # TODO: send over msgq to device
+
+      pc_latency = (time.monotonic()-time_q[0])*1000
+      time_q = time_q[1:]
+      if debug:
+        print("%2d %4d %.3f %.3f roll %6.2f ms latency %6.2f ms + %6.2f ms + %6.2f ms = %6.2f ms"
+              % (len(msgs), evta.idx.encodeId, evt.logMonoTime/1e9, evta.idx.timestampEof/1e6, frame_latency,
+                 process_latency, network_latency, pc_latency, process_latency+network_latency+pc_latency ), len(evta.data), sock_name)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description='')
+  parser.add_argument("addr", help="Address of comma three")
+  args = parser.parse_args()
+  socket_name = 'driverEncodeData'
+  # TODO: download from from https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5n.onnx
+  # place it in openpilot/tools/bodyteleop/models
+  yolo_runner = YoloRunner(....)
+  yolo_processor(args.addr, socket_name, yolo_runner, nvidia=False, debug=False)
---
+++ tools/bodyteleop/yolo_helpers.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+
+from openpilot.selfdrive.modeld.runners.onnxmodel import create_ort_session
+
+CLASSES = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+def xywh2xyxy(x):
+  y = x.copy()
+  y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+  y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+  y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+  y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+  return y
+
+def non_max_suppression(boxes, scores, threshold):
+  assert boxes.shape[0] == scores.shape[0]
+  ys1 = boxes[:, 0]
+  xs1 = boxes[:, 1]
+  ys2 = boxes[:, 2]
+  xs2 = boxes[:, 3]
+  areas = (ys2 - ys1) * (xs2 - xs1)
+  scores_indexes = scores.argsort().tolist()
+  boxes_keep_index = []
+  while len(scores_indexes):
+    index = scores_indexes.pop()
+    boxes_keep_index.append(index)
+    if not len(scores_indexes):
+      break
+    ious = compute_iou(boxes[index], boxes[scores_indexes], areas[index],
+            areas[scores_indexes])
+    filtered_indexes = set((ious > threshold).nonzero()[0])
+    scores_indexes = [
+      v for (i, v) in enumerate(scores_indexes)
+      if i not in filtered_indexes
+    ]
+  return np.array(boxes_keep_index)
+
+def compute_iou(box, boxes, box_area, boxes_area):
+  assert boxes.shape[0] == boxes_area.shape[0]
+  ys1 = np.maximum(box[0], boxes[:, 0])
+  xs1 = np.maximum(box[1], boxes[:, 1])
+  ys2 = np.minimum(box[2], boxes[:, 2])
+  xs2 = np.minimum(box[3], boxes[:, 3])
+  intersections = np.maximum(ys2 - ys1, 0) * np.maximum(xs2 - xs1, 0)
+  unions = box_area + boxes_area - intersections
+  ious = intersections / unions
+  return ious
+
+def nms(prediction, conf_thres=0.3, iou_thres=0.45):
+  prediction = prediction[prediction[...,4] > conf_thres]
+  boxes = xywh2xyxy(prediction[:, :4])
+  res = non_max_suppression(boxes,prediction[:,4],iou_thres)
+  result_boxes = []
+  result_scores = []
+  result_classes = []
+  for r in res:
+    result_boxes.append(boxes[r])
+    result_scores.append(prediction[r,4])
+    result_classes.append(np.argmax(prediction[r,5:]))
+  return np.c_[result_boxes, result_scores, result_classes]
+
+class YoloRunner:
+  def __init__(self, onnx_path):
+    self.sess = create_ort_session(onnx_path)
+
+  # TODO: add crop to center
+  def preprocess_image(self, img):
+    img = cv2.resize(img, (640, 640))
+    img = np.expand_dims(img, 0).astype(np.float32)
+    img = img.transpose(0, 3, 1, 2)
+    img /= 255
+    return img
+
+  def run(self, img):
+    img = self.preprocess_image(img)
+    res = self.sess.run(None, {'image': img})
+    res = nms(res[0])
+    return [{"pred_class": CLASSES[int(opt[-1])], "prob": opt[-2], "pt1": opt[:2].astype(int).tolist(), "pt2": opt[2:4].astype(int).tolist()} for opt in res]
