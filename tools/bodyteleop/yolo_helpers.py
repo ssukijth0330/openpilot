3b342c73efdd31834ca95ba1b24ff734809e3cf2
Yassine <yassine.y10@gmail.com>
2023-09-16 15:20:46 -0700

    another one

--- tools/bodyteleop/yolo_helpers.py
+++ tools/bodyteleop/yolo_helpers.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from openpilot.selfdrive.modeld.runners.onnxmodel import create_ort_session
 
+# TODO: use the names metadata from the onnx model to avoid this huge list
 CLASSES = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
 def xywh2xyxy(x):
