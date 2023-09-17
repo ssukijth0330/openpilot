0b30df17978928c2ebf08e61b58918e4b5915223
ZwX1616 <zwx1616@gmail.com>
2023-09-16 18:17:34 -0700

    should work

--- tools/bodyteleop/bodycontrolsd.py
+++ tools/bodyteleop/bodycontrolsd.py
@@ -1,15 +1,18 @@
 import json
+import argparse
 from cereal import messaging
 
-def main():
+def main(pc_addr):
   pm = messaging.PubMaster(['testJoystick'])
-  sm = messaging.SubMaster(['bodyReserved0'])
+  sm = messaging.SubMaster(['bodyReserved0'], addr=pc_addr)
 
   while True:
       sm.update(0)
 
       if sm.updated['bodyReserved0']:
           controls = json.loads(sm['bodyReserved0'])
+          # yolodat = json.loads(sm['bodyReserved0'])
+          # controls = f(yolodat)
           msg = messaging.new_message('testJoystick')
           msg.testJoystick.axes = [controls['x'], controls['y']]
           msg.testJoystick.buttons = [False]
@@ -17,4 +20,7 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
+  parser = argparse.ArgumentParser(description='')
+  parser.add_argument("addr", help="Address of pc")
+  args = parser.parse_args()
+  main(args.addr)
